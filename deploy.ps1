31c3ffb5212c43f63fe2074eb4366bcd0ea78b2b
Mimbles <adp851@ntlworld.com>
2020-11-28 17:41:14 +0000

    Added script to compile & deploy contracts as well as generate the java classes

--- deploy.ps1
+++ deploy.ps1
@@ -4,6 +4,8 @@ truffle deploy
 Write-Host "Creating java classes from smart contracts:"
 cd ..
 web3j truffle generate .\Truffle\build\contracts\MetaCoin.json -o .\Java\src\ -p sol
+web3j truffle generate .\Truffle\build\contracts\ConvertLib.json -o .\Java\src\ -p sol
+web3j truffle generate .\Truffle\build\contracts\Migrations.json -o .\Java\src\ -p sol
 Write-Host "Done"
 
  
